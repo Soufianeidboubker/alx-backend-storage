51e3d7f3632a7328a2bb1c2c718f13940ccf7c7d
soufianeidboubker <soufianeidboubker578@gmail.com>
2024-03-21 16:12:34 +0000

    update

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,4 +1,5 @@
-s-- creates an index idx_name_first_score
--- on the table names and the first letter of name and the score.
+-- Creates an index idx_name_first_score on table names and first letter of name and score
+-- Only first letter of name AND score must be indexed
+
 CREATE INDEX idx_name_first_score
 ON names(name(1), score);
